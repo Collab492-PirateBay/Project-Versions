38cd0d6e0ba0865e3a71400040a3850e466c744e
Alex <alex.agcaoili11@gmail.com>
2018-07-24 09:45:12 -0700

    AR image targets and UI

---
+++ PirateBay/Assets/Scripts/AR Scripts/AR_body.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AR_body : MonoBehaviour {
+
+	public ARmarker fairyspawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+			fairyspawn.enabled = true;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+			fairyspawn.enabled = false;
+	}
+}
